a1f6847b20fc87a201aa57dcda4fd461a7bf0bc3
JeongHoon Kim <devkjh@gmail.com>
2019-05-20 17:24:32 +0800

    add by id

--- src/app/services/sy-chinalife.service.ts
+++ src/app/services/sy-chinalife.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { ChinaLife } from '../sy-schemas/chinalife';
 import { CHINALIFES } from '../sy-mock/chinalife';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -16,8 +17,21 @@ export class SyChinalifeService {
     private http: HttpClient
   ) { }
 
+  private log(message: string){
+    
+  }
+
   getChinaLife(): Observable<ChinaLife[]>{
-    return this.http.get<ChinaLife[]>(this.chinaLifeUrl);
+    return this.http.get<ChinaLife[]>(this.chinaLifeUrl)
+    .pipe(catchError(this.handleError<ChinaLife[]>('getChinaLife', [])));
+  }
+
+  getChinaLifeById(id: Number): Observable<ChinaLife>{
+    const url = `${this.chinaLifeUrl}/${id}`;
+    return this.http.get<ChinaLife>(url).pipe(
+      tap(_ => this.log(`fetched chinallife id=${id}`)),
+      catchError(this.handleError<ChinaLife>(`getChinaLife id=${id}`))
+    );
   }
 
   getChinaLifeRx(): Observable<ChinaLife[]> {
@@ -26,5 +40,12 @@ export class SyChinalifeService {
   
   getChinaLifeByStatic(): ChinaLife[]{
     return CHINALIFES;
+  }
+
+  private handleError<T> (operators = 'operation', result? : T) {
+    return (error: any): Observable<T> => {
+      console.error(`sy-chinalife.service.ts -> `+error);
+      return of(result as T);
+    }
   }
 }
